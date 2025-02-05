ec4e3c7b65da8cecf2fe19a1de8bdb0db9ec494f
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-02-05 23:24:37 +0545

    Basic changes

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ const stopButton = document.getElementById('stop-btn').addEventListener('click',
     let myAudio = new Audio('./beep-104060.mp3');
     myAudio.play()
 });
+
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const microSecond = document.getElementById('microsecond');
