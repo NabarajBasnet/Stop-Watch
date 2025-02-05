d8a43fd132ce8121b36f6d3f111b0a202ad0a77a
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-02-05 22:57:46 +0545

    Icon and sound added in stop button

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ startButton.addEventListener('click', () => {
     let myAudio = new Audio('./beep-104060.mp3');
     myAudio.play()
 });
+
+
+
+const stopButton = document.getElementById('stop-btn');
+
+stopButton.addEventListener('click', () => {
+    let myAudio = new Audio('./beep-104060.mp3');
+    myAudio.play()
+});
