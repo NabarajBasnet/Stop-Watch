1ba127b508cc2ed81437b6347ffaef3389ef2c46
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2025-02-07 12:25:17 +0545

    Increating variables

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-const startButton = document.getElementById('start-btn').addEventListener('click', () => {
-    let myAudio = new Audio('./beep-104060.mp3');
-    myAudio.play()
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+let milisecond = 0;
+let timer = false;
+
+startBtn.addEventListener('click', () => {
+    timer = true;
+    const myAudio = new Audio('./beep-104060.mp3');
+    myAudio.play();
+    setInterval(() => watch(), 10);
 });
 
-const stopButton = document.getElementById('stop-btn').addEventListener('click', () => {
-    let myAudio = new Audio('./beep-104060.mp3');
-    myAudio.play()
+stopBtn.addEventListener('click', () => {
+    const myAudio = new Audio('./beep-104060.mp3');
+    myAudio.play();
+    timer = false;
 });
 
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-const microSecond = document.getElementById('microsecond');
+const watch = () => {
+    if (timer) {
+        milisecond++;
+        document.getElementById('milisecond').innerHTML = milisecond;
+    };
+
+    if (timer && milisecond === 100) {
+        milisecond = 0;
+        second++;
+        document.getElementById('second').innerHTML = second;
+    };
+
+    if (timer && second === 60) {
+        second = 0;
+        minute++;
+        document.getElementById('minute').innerHTML = minute;
+    };
+
+    if (timer && minute === 60) {
+        minute = 0;
+        hour++;
+        document.getElementById('hour').innerHTML = hour;
+    };
+
+    if (timer && hour === 12) {
+        hour = 1;
+        document.getElementById('hour').innerHTML = hour;
+    };
+};
