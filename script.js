23156fa9279d03160305868ac494a94cfcd3a02b
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-02-07 14:31:51 +0545

    pull

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-const startButton = document.getElementById('start-btn').addEventListener('click', () => {
+let startButton = document.getElementById('start-btn');
+let stopButton = document.getElementById('stop-btn');
+let resetButton = document.getElementById('reset-btn');
+
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+
+startButton.addEventListener('click', () => {
     let myAudio = new Audio('./beep-104060.mp3');
-    myAudio.play()
+    myAudio.play();
+
+    timer = true;
+    stopWatch();
 });
 
-const stopButton = document.getElementById('stop-btn').addEventListener('click', () => {
+const stop = stopButton.addEventListener('click', () => {
     let myAudio = new Audio('./beep-104060.mp3');
-    myAudio.play()
+    myAudio.play();
+
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+
+    document.getElementById('minute').innerHTML = "00";
+    document.getElementById('second').innerHTML = "00";
+    document.getElementById('microsecond').innerHTML = "00";
 });
 
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-const microSecond = document.getElementById('microsecond');
+const onStart = document.getElementById('start-btn').addEventListener('click', () => {
+    setInterval(() => {
+        count = count + 1;
+    }, 1000);
+    minute.innerText = count;
+});
